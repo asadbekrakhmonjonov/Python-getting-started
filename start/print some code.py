e1e2fb6b00a4a12b7e74ccd3fd2223445298e13e
asadbekrakhmonjonov <143370851+asadbekrakhmonjonov@users.noreply.github.com>
2023-11-19 21:12:13 +0900

    Add files via upload

---
+++ start/print some code.py
@@ -0,0 +1,7 @@
+"""Thus far, you have probably used double quotation marks " to print out strings. In addition to the double quotation marks, Python also accepts single quotation marks '.
+
+This comes in handy if you ever want to print out the actual quotation marks themselves: """
+
+# Write your solution here
+print('print("Hello there!")')
+
