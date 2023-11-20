4b91a58a9ce901d37b48a06d5198e29f448235f6
asadbekrakhmonjonov <143370851+asadbekrakhmonjonov@users.noreply.github.com>
2023-11-20 15:04:39 +0900

    Add files via upload

---
+++ more about variables/arithmetics.py
@@ -0,0 +1,16 @@
+""" This program already contains two integer variables, x and y:
+ the program should print out the following:
+
+Sample output
+4 + 9 = 13
+4 - 9 = -5
+4 * 9 = 36
+4 / 9 = 0.4444444444444444"""
+
+# Write your solution here
+x = 27
+y = 15
+print(f"{x} + {y} =",x+y)
+print(f"{x} - {y} =",x-y)
+print(f"{x} * {y} =",x*y)
+print(f"{x} / {y} =",x/y)
---
+++ more about variables/extra space.py
@@ -0,0 +1,41 @@
+""" Your friend is working on an app for jobseekers. She sends you this bit of code:
+
+name = "Tim Tester"
+age = 20
+skill1 = "python"
+level1 = "beginner"
+skill2 = "java"
+level2 = "veteran"
+skill3 = "programming"
+level3 = "semiprofessional"
+lower = 2000
+upper = 3000
+
+print("my name is ", name, " , I am ", age, "years old")
+print("my skills are")
+print("- ", skill1, " (", level1, ")")
+print("- ", skill2, " (", level2, ")")
+print("- ", skill3, " (", level3, " )")
+print("I am looking for a job with a salary of", lower, "-", upper, "euros """
+
+#correct the code
+
+name = "Tim Tester"
+age = 20
+skill1 = "python"
+level1 = "beginner"
+skill2 = "java"
+level2 = "veteran"
+skill3 = "programming"
+level3 = "semiprofessional"
+lower = 2000
+upper = 3000
+
+print("my name is", name + ",","I am", age, "years old")
+print("")
+print("my skills are")
+print(" -", skill1, "(" + level1 + ")")
+print(" -", skill2, "(" + level2 + ")")
+print(" -", skill3, "(" + level3 + ")")
+print("")
+print("I am looking for a job with a salary of", str(lower) + "-" + str(upper), "euros per month")
